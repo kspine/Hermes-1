d8acfe2a6cba5552fe00f94b937a79f048a72410
Tommy <thomas.milox@epitech.eu>
2017-02-06 14:03:52 +0100

    [windows] 		- initial commit.

--- tests/windows/main.cpp
+++ tests/windows/main.cpp
@@ -1,9 +1,16 @@
 #include "Hermes.hpp"
 
 using namespace hermes;
+using namespace hermes::tools;
 using namespace hermes::network;
 
-void tests_tcp_socket(void) { std::cout << "Working :)\n"; }
+void tests_tcp_socket(void) {
+  tcp::socket socket;
+
+  assert(socket.get_fd() == INVALID);
+  assert(socket.get_host() == "");
+  assert(socket.get_port() == 0);
+}
 
 int __cdecl main(void) {
   tests_tcp_socket();
